07588a5a3d0013482c866464cbab7e8ac98b5004
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-19 01:38:45 -0500

    feat: Ajuste de los Arrays de Producto

--- src/main/java/org/example/Datos.java
+++ src/main/java/org/example/Datos.java
@@ -13,10 +13,32 @@ public class Datos {
     public static final String direccion = "Calle 15 #8-32, Valledupar";
     public static final String nit = "900.123.456-7";
 
+    private static final Producto[] catalogo = {
+            new Producto("Bandeja Paisa",       32000),
+            new Producto("Sancocho de Gallina", 28000),
+            new Producto("Arepa con Huevo",      8000),
+            new Producto("Jugo Natural",         7000),
+            new Producto("Gaseosa",              4500),
+            new Producto("Cerveza Poker",        6000),
+            new Producto("Agua Panela",          3500),
+            new Producto("Arroz con Pollo",     25000),
+    };
+
     private static int    numeroMesa    = 0;
     private static int    estadoMesa    = 0;   // 0 = libre, 1 = activa
     private static double totalFactura  = 0;
     private static int    numeroFactura = 1;
+
+    //Constructor privado para que no se pueda instanciar un new Datos
+    private Datos() { }
+
+    public static Producto[] getCatalogo() {
+        return catalogo;
+    }
+
+    public static int getTotalProductosEnCarta() {
+        return catalogo.length;
+    }
 
     public static int getNumeroMesa() {
         return numeroMesa;
