35472b130627e3845d14bb614df13186655da194
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-19 01:16:24 -0500

    feat: Creación atributos referentes a mesa y factura en Datos.java

--- src/main/java/org/example/Datos.java
+++ src/main/java/org/example/Datos.java
@@ -12,4 +12,41 @@ public class Datos {
     public static final String nombreRestaurante = "Restaurante El Buen Sabor";
     public static final String direccion = "Calle 15 #8-32, Valledupar";
     public static final String nit = "900.123.456-7";
+
+    private static int    numeroMesa    = 0;
+    private static int    estadoMesa    = 0;   // 0 = libre, 1 = activa
+    private static double totalFactura  = 0;
+    private static int    numeroFactura = 1;
+
+    public static int getNumeroMesa() {
+        return numeroMesa;
+    }
+
+    public static void setNumeroMesa(int numeroMesa) {
+        Datos.numeroMesa = numeroMesa;
+    }
+
+    public static int getEstadoMesa() {
+        return estadoMesa;
+    }
+
+    public static void setEstadoMesa(int estadoMesa) {
+        Datos.estadoMesa = estadoMesa;
+    }
+
+    public static double getTotalFactura() {
+        return totalFactura;
+    }
+
+    public static void setTotalFactura(double totalFactura) {
+        Datos.totalFactura = totalFactura;
+    }
+
+    public static int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public static void setNumeroFactura(int numeroFactura) {
+        Datos.numeroFactura = numeroFactura;
+    }
 }
