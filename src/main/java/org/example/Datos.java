eb91ec300300b9839fae6aeeb5747080ee8fb482
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-19 00:29:38 -0500

    feat: División de las funciones en el metodo hacerTodo en Proceso.java

---
+++ src/main/java/org/example/Datos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+/**
+ *
+ * @author alfre
+ */
+public class Datos {
+    public static final String nombreRestaurante = "Restaurante El Buen Sabor";
+    public static final String direccion = "Calle 15 #8-32, Valledupar";
+    public static final String nit = "900.123.456-7";
+}
---
+++ src/main/java/org/example/Proceso.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+/**
+ *
+ * @author alfre
+ */
+public class Proceso {
+
+    //En este taller las funciones son public static porque no se instancia ninguna clase
+    public static double calcularTotal(){
+
+    }
+
+    public static double aplicarDescuento(){
+
+    }
+
+    public static double calcularSubtotal(){
+
+    }
+
+}
