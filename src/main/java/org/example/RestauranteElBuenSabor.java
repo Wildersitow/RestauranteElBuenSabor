e5d7446138f97f8bb41280263cafef6a4530f7ef
wpverdecia <wpverdecia@unicesar.edu.co>
2026-04-19 16:26:03 -0500

    feat: Creación metodo imprimirMenuPrincipal

--- src/main/java/org/example/RestauranteElBuenSabor.java
+++ src/main/java/org/example/RestauranteElBuenSabor.java
@@ -16,7 +16,12 @@ public class RestauranteElBuenSabor {
 
     public static void main(String[] args) {
         imprimirBienvenida();
+        boolean ejecutando = true;
 
+        while (ejecutando) {
+            imprimirMenuPrincipal();
+            
+        }
         sc.close();
     }
 
@@ -25,6 +30,16 @@ public class RestauranteElBuenSabor {
         System.out.println("    " + Datos.nombreRestaurante.toUpperCase());
         System.out.println("    " + Datos.direccion);
         System.out.println("    NIT: " + Datos.nit);
+        System.out.println("========================================");
+    }
+
+    private static void imprimirMenuPrincipal() {
+        System.out.println("1. Ver carta");
+        System.out.println("2. Agregar producto al pedido");
+        System.out.println("3. Ver pedido actual");
+        System.out.println("4. Generar factura");
+        System.out.println("5. Nueva mesa");
+        System.out.println("0. Salir");
         System.out.println("========================================");
     }
 
